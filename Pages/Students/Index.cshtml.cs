2da377a476af416a4077b2164b65912cab5ea3cd
Sammy Gaskins <sammycole@comcast.net>
2021-10-22 13:56:25 -0500

    Added sorting by name age and date

--- Pages/Students/Index.cshtml.cs
+++ Pages/Students/Index.cshtml.cs
@@ -12,18 +12,53 @@ namespace csci340lab8.Pages.Students
 {
     public class IndexModel : PageModel
     {
-        private readonly csci340lab8.Data.SchoolContext _context;
-
-        public IndexModel(csci340lab8.Data.SchoolContext context)
+        private readonly SchoolContext _context;
+        public IndexModel(SchoolContext context)
         {
             _context = context;
         }
 
-        public IList<Student> Student { get;set; }
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string AgeSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        public IList<Student> Students { get; set; }
+
+        public async Task OnGetAsync(string sortOrder)
         {
-            Student = await _context.Students.ToListAsync();
+            // using System;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            AgeSort = sortOrder == "Age" ? "age_desc" : "Age";
+
+            IQueryable<Student> studentsIQ = from s in _context.Students
+                                             select s;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                case "Age":
+                    studentsIQ = studentsIQ.OrderBy(s => s.Age);
+                    break;
+                case "age_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.Age);
+                    break;
+                default:
+                    studentsIQ = studentsIQ.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            Students = await studentsIQ.AsNoTracking().ToListAsync();
         }
     }
 }
