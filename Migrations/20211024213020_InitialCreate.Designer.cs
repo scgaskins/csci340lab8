297b59c5dbc9a0d901d076d2657620bc1e4554b1
Sammy Gaskins <sammycole@comcast.net>
2021-10-24 16:33:56 -0500

    Completed Step 5

--- Migrations/20211024213020_InitialCreate.Designer.cs
+++ Migrations/20211024213020_InitialCreate.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using csci340lab8.Data;
 
 namespace csci340lab8.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20211024213020_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
